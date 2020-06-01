30d3d43a52197e9ee74336d63d68e2eeb4d273bc
Nils-Lopez <nils.lpzz@gmail.com>
2020-06-01 14:45:00 -0700

    With a user show view

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -1,11 +1,13 @@
 class PaymentMailer < ApplicationMailer
 	default from: 'dev.hellokitties@gmail.com'
  
-  def payment_confirmation
-  	#@user = current_user
+  def payment_confirmation(item)
+  	@user = current_user
 
   	#@url = "monsite.herokuapp.com"
 
-  	mail(to: "nils.lpzz@gmail.com", subject: "Confirmation de votre commande")
+  	@item = item
+
+  	mail(to: @user.email, subject: "Confirmation de votre commande")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'user/show'
   devise_for :users
   resources :items
   root "items#index"
-
+  resources :user, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
