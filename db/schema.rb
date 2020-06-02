1289788d45a52b5c5a66aa5870b3a9baeb1bbb0f
corentinpierru <corentin.pierru@laposte.net>
2020-06-02 21:26:34 +0200

    modif db order items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_141648) do
+ActiveRecord::Schema.define(version: 2020_06_02_192324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,12 +33,20 @@ ActiveRecord::Schema.define(version: 2020_06_02_141648) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.bigint "user_id"
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id"
     t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "stripe_customer_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
