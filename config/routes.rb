e5a479c217193718967d9353423cff3110c89bc6
DENEUBOURG Cedric <deneubourg.cedric@thedigitalfab.com>
2020-06-03 18:29:07 +0200

    Correct Git error

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,13 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-
-  
-  get 'user/show'
-  devise_for :users
-  resources :items do
-  	resources :pictures, only: [:create, :update]
-  end
-=======
->>>>>>> a2465e8087add042365722bf8c4a1bfd4f3dfcbf
   root "items#index"
   devise_for :users
   resources :user, only: [:show]
-<<<<<<< HEAD
-  resources :admin, only: [:edit, :create, :destroy]
-
-=======
-  resources :items
+  get 'user/show'
+  resources :items do
+  	resources :pictures, only: [:create, :update]
+  end
   resources :carts
   resources :charges
->>>>>>> a2465e8087add042365722bf8c4a1bfd4f3dfcbf
+  resources :admin, only: [:edit, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
